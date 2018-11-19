0f72a7e451c4e69d222715a55418d175dfc41a30
pavangudur <pavankumarga@gmail.com>
2018-11-19 00:35:23 -0600

    Updated with Welcome Screen
    UI of Welcome Screen is completed.

---
+++ LoginScreen/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  LoginScreen
+//
+//  Created by Pavan Gudur on 11/18/18.
+//  Copyright © 2018 Pavan Gudur. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signUpButton(_ sender: UIButton) {
+    }
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+    }
+    
+}
